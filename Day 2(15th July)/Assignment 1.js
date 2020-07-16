23908216a49696a6c4f3802a8efa435bcd1dc9b4
Pragati Kumari <44642938+mepragati@users.noreply.github.com>
2020-07-16 13:52:13 +0530

    Add files via upload

---
+++ Day 2(15th July)/Assignment 1.js
@@ -0,0 +1,4 @@
+/*Question 1:
+Write a JS code which takes input from the user and logs it in the console.*/
+var name = window.prompt("Enter your name: ");
+console.log("Welcome!! " + name);
---
+++ Day 2(15th July)/Assignment 3.js
@@ -0,0 +1,13 @@
+/*Question 3:
+Ask the user if he is 21+ and log the value "Can drink", "Cannot Drink".*/
+
+var age = window.prompt("Enter your age: ");
+if(age> 21)
+{
+    console.log("Can Drink");
+}
+
+else
+{
+    console.log("Cannot Drink");
+}
